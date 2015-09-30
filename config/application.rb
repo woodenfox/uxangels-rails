f81d3665b0175d688dd836c079b4fc355a156389
Mike Messenger <woodenfox@gmail.com>
2015-09-30 11:09:11 -0700

    add jQuery layout tags

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,7 @@ module UxangelsRails
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    # jquery-rails-cdn (remove on delete)
+    config.assets.precompile += ['jquery.js']
   end
 end
