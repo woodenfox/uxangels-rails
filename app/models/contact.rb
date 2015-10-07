0344de78fa1305286c36de1341f8f1e6a30c8c20
Mike Messenger <woodenfox@gmail.com>
2015-10-06 22:02:17 -0700

    Minor tweaks

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,8 @@
+require 'cgi' #Required to pull in URL Paremeters
+
 class Contact < MailForm::Base
+  #params = CGI.parse(URI.parse("http://www.cat.com/?cat=CatGo&dog=2").query)
+    
   # attribute :name,      :validate => true
   attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
   attribute :message
@@ -6,9 +10,11 @@ class Contact < MailForm::Base
 
   # Declare the e-mail headers. It accepts anything the mail method
   # in ActionMailer accepts.
+  
+  
   def headers
     {
-      :subject => "UXA Contact",
+      :subject => "Contact from UX Angels",
       :to => "woodenfox@gmail.com",
       :from => %(<#{email}>)
       # :from => %("#{name}" <#{email}>)
